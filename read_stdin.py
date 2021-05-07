c2efa12e5e8e8b4006c48d652a05025ac8af8aae
Vikora <vikora@yandex.ru>
2021-05-07 23:04:00 +0300

    Read int array

--- read_stdin.py
+++ read_stdin.py
@@ -1,17 +1,16 @@
-"""
-Read input from stdin.
-Input format:
-n m
-n lines of int-arrays of size m
-e.g.
->>> 2 3
->>> 9 8 7
->>> 4 5 6
-"""
-
 import sys
 
-def main():
+def read_matrix():
+    """
+        Read input from stdin.
+        Input format:
+        n m
+        n lines of int-arrays of size m
+        e.g.
+        >>> 2 3
+        >>> 9 8 7
+        >>> 4 5 6
+    """
     first_line = sys.stdin.readline()
     n, m = map(int, first_line.split())
     print(f"$ {n} {m}")
@@ -19,5 +18,10 @@ def main():
         m_array = list(map(int, line.split()))
         print(f"$ {m_array}")
 
+def read_array():
+    n = int(sys.stdin.readline())
+    line = sys.stdin.readline().split()
+    array = list(map(int, line))
+
 if __name__ == "__main__":
-    main()
+    read_matrix()
